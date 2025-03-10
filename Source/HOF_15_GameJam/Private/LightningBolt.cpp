8709fc1e6867570b44842362764ef9585ba9dbf9
Michael McGurk <themcgurkfiles@gmail.com>
2025-03-09 22:34:54 -0400

    Lightning bolt started

---
+++ Source/HOF_15_GameJam/Private/LightningBolt.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LightningBolt.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ALightningBolt::ALightningBolt()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+
+    // Create Mesh
+    MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+    RootComponent = MeshComponent;
+
+    // Create Projectile Movement
+    ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+    ProjectileMovement->ProjectileGravityScale = 1.0f;
+    ProjectileMovement->InitialSpeed = 0.0f;
+    ProjectileMovement->MaxSpeed = 5000.0f;
+    ProjectileMovement->bShouldBounce = false;
+
+    // Enable collision handling
+    MeshComponent->SetSimulatePhysics(false);
+    MeshComponent->SetCollisionProfileName(TEXT("BlockAll"));
+    MeshComponent->OnComponentHit.AddDynamic(this, &ALightningBolt::OnHit);
+}
+
+// Called when the game starts or when spawned
+void ALightningBolt::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ALightningBolt::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ALightningBolt::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+    if (OtherActor && OtherActor != this)
+    {
+        // Example damage
+        UGameplayStatics::ApplyDamage(OtherActor, 1.0f, GetInstigatorController(), this, nullptr);
+        Destroy();
+    }
+}
+
---
+++ Source/HOF_15_GameJam/Public/LightningBolt.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/StaticMesh.h"
+#include "LightningBolt.generated.h"
+
+UCLASS()
+class HOF_15_GAMEJAM_API ALightningBolt : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ALightningBolt();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+
+    // Components
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UStaticMeshComponent* MeshComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    class UProjectileMovementComponent* ProjectileMovement;
+
+    // Collision event
+    UFUNCTION()
+    void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, FVector NormalImpulse,
+        const FHitResult& Hit);
+};
